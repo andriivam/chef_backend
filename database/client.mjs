b7c4b7716a7dd102d7e71cfafc090e861e0e82c5
andriivam <andriivlasiuk2@gmail.com>
2023-02-05 14:54:18 +0100

    test

--- database/client.mjs
+++ database/client.mjs
@@ -4,23 +4,6 @@ import pkg from 'pg';
 dotenv.config()
 const { Client } = pkg;
 
-
-// const client = new Client({
-// connectionString: process.env.DATABASE_URL || "development",
-// ssl: {
-//     rejectUnauthorized: false
-// }
-
-// });
-
-// client.connect((err) => {
-// if (err) {
-//     console.error('connection error', err.stack)
-// } else {
-//     console.log('connected')
-// }
-// })
-
 const client = (() => {
     if (process.env.NODE_ENV !== 'production') {
         return new Client({
